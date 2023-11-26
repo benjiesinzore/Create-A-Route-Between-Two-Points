a640e9ab0dd50b08ac7cacdaff8d2513a8137fe1
Benjamin Sinzore <benjaminsinzore@gmail.com>
2023-11-27 02:41:19 +0300

    Worked on Google Maps Route directions

--- app/src/main/java/com/sinzorebenjamin/routebetweentwopoints/MainActivity.java
+++ app/src/main/java/com/sinzorebenjamin/routebetweentwopoints/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        assert mapFragment != null;
+//        mapFragment.getMapAsync((OnMapReadyCallback) MainActivity.this);
     }
 }
